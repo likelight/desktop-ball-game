52d85eada7f444153d556b35b765a1345ed2893e
xiaoyunxiao <xiaoyunxiao@baidu.com>
2017-09-05 18:41:20 +0800

    improve proj directory structure

---
+++ js/button.js
@@ -0,0 +1,31 @@
+/**
+ * @file Button Class
+ * @author yunxiange@gmail.com
+ * @date 2017-09-05
+ */
+
+(function () {
+    function Button(options) {
+        //
+    }
+
+    var ButtonProto = Button.prototype;
+
+    ButtonProto.create = function () {
+        //
+    };
+
+    ButtonProto.init = function () {
+        //
+    };
+
+    ButtonProto.move = function () {
+        //
+    };
+
+    ButtonProto.getCood = function () {
+        //
+    };
+
+    window.Button = Button;
+})();
---
+++ js/util.js
@@ -0,0 +1,14 @@
+/**
+ * @file util methods
+ * @author yunxiange@gmail.com
+ * @date 2017-09-05
+ */
+
+(function () {
+    var util = {
+        //
+    };
+
+    window.util = util;
+
+})();
