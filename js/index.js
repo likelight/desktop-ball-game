63a098672b31556e2be4bdde746d29ed3e7c4595
xiaoyunxiao <xiaoyunxiao@baidu.com>
2017-09-06 23:36:00 +0800

    碰撞+骰子运动

--- js/index.js
+++ js/index.js
@@ -44,6 +44,10 @@
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight - 70;
 
+    var clientRect = canvas.getBoundingClientRect();
+    var directionX = 1;
+    var directionY = 1;
+
     var gameManager = {
         manifest: [
             {
@@ -69,7 +73,38 @@
             this.loader.addEventListener('complete', this.handleLoadComplete.bind(this));
             this.loader.loadManifest(this.manifest, true, './images/');
         },
-        tick: function () {
+        tick: function (e) {
+            var leftIntersection = ndgmr.checkPixelCollision(this.ball, this.leftButton, 0);
+            var rightIntersection = ndgmr.checkPixelCollision(this.ball, this.rightButton, 0);
+
+            if (leftIntersection) {
+                var xDistance = this.lastLeftButtonStyle.x - this.leftButtonShape.x;
+                var yDistance = this.lastLeftButtonStyle.y - this.leftButtonShape.y;
+                var angle = Math.atan(yDistance/xDistance);
+                var ballStyle = this.ball.getTransformedBounds();
+
+                this.ball.isMoving = true;
+            }
+
+            if (this.ball.isMoving) {
+                this.ball.x += 1 * directionX;
+                this.ball.y += 1 * directionY;
+
+                if (this.ball.x < 0) {
+                    directionX = 1;
+                }
+                if (this.ball.x > clientRect.width - this.ball.width) {
+                    directionX = -1;
+                }
+                if (this.ball.y > clientRect.height - this.ball.height) {
+                    directionY = -1;
+                }
+                if (this.ball.y < 0) {
+                    directionY = 1;
+                }
+
+                this.stage.update();
+            }
         },
         handleLoadComplete: function () {
             this.stage = new createjs.Stage(canvas);
@@ -89,52 +124,123 @@
             var buttonContainer = new createjs.Container();
 
             var buttonImg = this.loader.getResult('button');
-            var leftButton = new createjs.Bitmap(buttonImg);
-            var rightButton = new createjs.Bitmap(buttonImg);
-            leftButton.x = 10;
-            leftButton.y = 170;
+            this.leftButton = new createjs.Bitmap(buttonImg);
+            this.rightButton = new createjs.Bitmap(buttonImg);
 
-            rightButton.x = 850;
-            rightButton.y = 170;
-            rightButton.scaleX = leftButton.scaleX = canvas.width / bg1.getBounds().width;
-            rightButton.scaleY = leftButton.scaleY = canvas.height / (bg1.getBounds().height);
+            this.rightButton.scaleX = this.leftButton.scaleX = canvas.width / bg1.getBounds().width;
+            this.rightButton.scaleY = this.leftButton.scaleY = canvas.height / bg1.getBounds().height;
+
+            this.leftButton.x = 0.1 * clientRect.width;
+            this.leftButton.y = 0.5 * clientRect.height - this.leftButton.getBounds().height * 0.5;
+
+            this.rightButton.x = 0.8 * clientRect.width;
+            this.rightButton.y = 0.5 * clientRect.height - this.rightButton.getBounds().height * 0.5;
+
+            var ballImg = this.loader.getResult('ball');
+            this.ball = new createjs.Bitmap(ballImg);
+            // 随机出现
+            // ball.x = Math.random() * 0.5 * clientRect.width + 0.3 * clientRect.width;
+            // ball.y = Math.random() * 0.5 * clientRect.height + 0.3 * clientRect.height;
+            this.ball.x = 200;
+            this.ball.y = 150;
+            this.ball.isMoving = false;
+            this.ball.vx = 0;
+            this.ball.vy = 0;
+            this.ball.scaleX = canvas.width / bg1.getBounds().width;
+            this.ball.scaleY = canvas.height / bg1.getBounds().height;
+            this.ball.width = this.ball.getBounds().width;
+            this.ball.height = this.ball.getBounds().height;
+
+            this.lastLeftButtonStyle = this.leftButton.getTransformedBounds();
+            this.lastRightButtonStyle = this.rightButton.getTransformedBounds();
 
             //this.stage.addChild(Rect);//添加子视图
-            leftButton.addEventListener("mousedown", function (e) {
-                var shape = e.target;
+            this.leftButton.addEventListener("mousedown", function (e) {
+                this.lastLeftButtonStyle = this.leftButton.getTransformedBounds();
+                var initLeftButtonStyle = this.leftButton.getTransformedBounds();
+                var initEvtStyle = {
+                    x: e.stageX,
+                    y: e.stageY
+                };
+                var shape = this.leftButtonShape = e.target;
                 shape.on('pressmove', function (t) {
-                    shape.x = t.stageX;
-                    shape.y = t.stageY;
+                    shape.x = t.stageX - (initEvtStyle.x - initLeftButtonStyle.x);
+                    shape.y = t.stageY - (initEvtStyle.y - initLeftButtonStyle.y);
+
+                    var leftLimit = 20;
+                    var rightLimit = 0.5 * clientRect.width - initLeftButtonStyle.width;
+                    var topLimit = 12;
+                    var bottomLimit = clientRect.height - initLeftButtonStyle.height - 12;
+
+                    if (shape.x < leftLimit) {
+                        shape.x = leftLimit;
+                    }
+                    if (shape.x > rightLimit) {
+                        shape.x = rightLimit;
+                    }
+                    if (shape.y < topLimit) {
+                        shape.y = topLimit;
+                    }
+                    if (shape.y > bottomLimit) {
+                        shape.y = bottomLimit;
+                    }
+
                     this.stage.update();
                 }.bind(this));
 
                 shape.on("pressup", function(evt) {
-                     shape.removeAllEventListeners();
+                     this.lastLeftButtonStyle = shape.getTransformedBounds();
                 }.bind(this));
 
             }.bind(this));
 
-            rightButton.addEventListener("mousedown", function (e) {
-                var shape = e.target;
+            this.rightButton.addEventListener("mousedown", function (e) {
+                var rightButtonStyle = this.rightButton.getTransformedBounds();
+                var initEvtStyle = {
+                    x: e.stageX,
+                    y: e.stageY
+                };
+                var shape = this.rightButtonShape = e.target;
                 shape.on('pressmove', function (t) {
-                    shape.x = t.stageX;
-                    shape.y = t.stageY;
+                    shape.x = t.stageX - (initEvtStyle.x - rightButtonStyle.x);
+                    shape.y = t.stageY - (initEvtStyle.y - rightButtonStyle.y);
+
+                    var leftLimit = 0.5 * clientRect.width;
+                    var rightLimit = clientRect.width - rightButtonStyle.width - 15;
+                    var topLimit = 12;
+                    var bottomLimit = clientRect.height - rightButtonStyle.height - 12;
+
+                    if (shape.x < leftLimit) {
+                        shape.x = leftLimit;
+                    }
+                    if (shape.x > rightLimit) {
+                        shape.x = rightLimit;
+                    }
+                    if (shape.y < topLimit) {
+                        shape.y = topLimit;
+                    }
+                    if (shape.y > bottomLimit) {
+                        shape.y = bottomLimit;
+                    }
+
                     this.stage.update();
                 }.bind(this));
 
                 shape.on("pressup", function(evt) {
-                    shape.removeAllEventListeners();
+                    // shape.removeAllEventListeners();
                 }.bind(this));
 
             }.bind(this));
 
-            buttonContainer.addChild(leftButton);
-            buttonContainer.addChild(rightButton);
+            buttonContainer.addChild(this.ball);
+            buttonContainer.addChild(this.rightButton);
+            buttonContainer.addChild(this.leftButton);
 
             this.stage.addChild(bgContainer);
             this.stage.addChild(buttonContainer);
             this.stage.update();
         },
+
         startMove: function (e) {
             var shape = e.target;
             this.stage.addEventListener('stagemousemove', function (t) {
@@ -142,13 +248,12 @@
                 shape.x = t.stageX;
                 shape.y = t.stageY;
             });
-
         },
+
         startGame: function () {
-
         },
-        stopGame: function () {
 
+        stopGame: function () {
         }
     };
 
